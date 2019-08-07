2a5f9e4c86927ab44e96035baf85298ab7305fd1
0xcurb <17249022+0xcurb@users.noreply.github.com>
2019-08-07 17:51:21 -0400

    Create hash_to_values.rb

---
+++ _plugins/hash_to_values.rb
@@ -0,0 +1,12 @@
+module Jekyll
+  module ValuesFilter
+    def values(input)
+      case
+        when input.instance_of?(Hash)
+          input.values
+        else
+          input
+      end
+    end
+  end
+end
